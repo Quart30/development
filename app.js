868438670e5f38bc4998e7b331fa0ae06f97157c
Mac <randy.tran11@gmail.com>
2016-03-03 12:42:13 -0800

    trying to get automated slack integration

--- app.js
+++ app.js
@@ -359,9 +359,10 @@ app.delete('/deleteappointment', function(req, res) {
 
 app.get('/registerslack', function(req, res) {
 
-    var params = JSON.parse(req);
+    var params = req.query;
 
-    console.log(params);
+    console.log(params.access_token);
+    console.log(params.incoming_webhook);
 
     res.redirect('/businesssetting/'); // redirect after processing data
 });
