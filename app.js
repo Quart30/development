1bb87f233c18e135de242e72ef026f899cd4971c
Mac <randy.tran11@gmail.com>
2016-03-03 12:24:47 -0800

    trying to get automated slack integration

--- app.js
+++ app.js
@@ -361,7 +361,7 @@ app.get('/registerslack', function(req, res) {
 
     console.log(req);
 
-    res.redirect('/accountsettings/'); // redirect after processing data
+    res.redirect('/businesssettings/'); // redirect after processing data
 });
 
 
