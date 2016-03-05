f89d4293422695156f54fe43470fca9d8948cffe
Kyle Enfield <kenfield93@gmail.com>
2016-03-05 11:48:50 -0800

    Set up logic for saving forms. Need to save to db and handle error messages now

--- routes/webapp/business/save_form.js
+++ routes/webapp/business/save_form.js
@@ -1,16 +1,30 @@
+ var isUnique = function(formName,  nameList){
 
+     for(var i = 0; i < nameList.length; i++){
+         if( formName == nameList[i])
+            return false;
+     }
+     return true;
+ }
 
 exports.post = function (req, res) {
     var formData = (req.body.saveData);
     var formName = (req.body.formName);
+    formName = formName.trim();
     var db = req.db;
+    var formNameList;
     console.log("formName: " + formName + "\n formData: " + formData);
-    /*
-     if formName is unique and formData isn't empty
-     then insert formName and formData into db
-    else
-    propogate error to user
-    */
-    //console.log(req);
+
+    if( formName == null || formData == null){
+        //send error message, stay on same page
+    }
+    else if( !isUnique(formName, formNameList)) { // elif formName isn't unique
+        // send error message
+    }
+    else{
+        // function to prevent sql injection needed?
+        // add {formData : formName} to data base
+       //temp redirect to success page or just post success message
+    }
     res.render('business/formBuilder', {title: 'Express'});
 };
